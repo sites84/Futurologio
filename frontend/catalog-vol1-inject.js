7bedd6cc8ea07947576ee82677ee117d4b7764d3
sites84 <edsonfernandesvet@gmail.com>
2026-09-13 16:07:12 -0300

    catalog: disable legacy catalog loaders globally

--- frontend/catalog-vol1-inject.js
+++ frontend/catalog-vol1-inject.js
@@ -1,5 +1,6 @@
 (()=>{'use strict';
-// Legacy Volume I injector retired: the single master catalog is authoritative.
+// Legacy catalog injectors are retired. The single master catalog is authoritative.
 window.__FUTURO_CATALOG_UNION_V4=true;
+window.__FUTURO_LEGACY_CATALOG_LOADERS_DISABLED=true;
 const s=document.createElement('script');s.src='./frontend/catalog-master-loader.js?v=20260913a';document.head.appendChild(s);
 })();
