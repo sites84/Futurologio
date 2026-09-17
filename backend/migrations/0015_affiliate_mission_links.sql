3d83aa115c2cf08463c55ad625b8b31fc1681919
sites84 <edsonfernandesvet@gmail.com>
2026-09-17 00:18:43 -0300

    feat: store daily affiliate mission destinations

---
+++ backend/migrations/0015_affiliate_mission_links.sql
@@ -0,0 +1,3 @@
+-- FUTUROLOGIO™: guarda os dois destinos Mercado Livre sorteados por usuário/dia
+ALTER TABLE DAILY_CREDIT_MISSIONS ADD COLUMN ad_link_2 TEXT;
+ALTER TABLE DAILY_CREDIT_MISSIONS ADD COLUMN ad_link_3 TEXT;
